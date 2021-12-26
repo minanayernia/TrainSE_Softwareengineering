58abced039c0748465717f490f7a92fee1930af5
zeinab kamkar <zeinabkamkar4@gmail.com>
2021-12-26 16:22:22 +0330

    build done

--- template/src/constants/config.js
+++ template/src/constants/config.js
@@ -5,7 +5,7 @@ export const adminRoot = '/app';
 export const searchPath = `${adminRoot}/pages/miscellaneous/search`
 export const buyUrl = 'https://1.envato.market/nEyZa'
 export const apiUrl = 'https://api.coloredstrategies.com';
-export const api = 'http://172.16.205.12:8000/';
+export const api = 'http://127.0.0.1:8000/';
 
 export const subHiddenBreakpoint = 1440
 export const menuHiddenBreakpoint = 768
